1a79c87da6527f524776ff34f422ff18cd65a411
Jack Franklin <jack@jackfranklin.net>
2016-02-07 21:44:49 +0000

    Generate script tags

--- src/script.js
+++ src/script.js
@@ -1,2 +1,12 @@
+import { Node, textNode, toHtml } from './html';
+
 export default function({ src, contents }) {
+  let node;
+  if (contents) {
+    node = new Node('script', {}, [textNode(contents)]);
+  } else {
+    node = new Node('script', { src }, []);
+  }
+
+  return toHtml(node);
 };
